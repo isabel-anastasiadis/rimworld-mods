fe4e76c5266656c5c1b33ea5184d100eab4b3b5c
Isabel Anastasiadis <anastasiadis.isabel@gmail.com>
2021-08-19 17:52:59 +1200

    tidied up, hopefully got the drying working alright

--- BloodDries/1.3/Source/Izzimon.BloodDries/Blood.cs
+++ BloodDries/1.3/Source/Izzimon.BloodDries/Blood.cs
@@ -8,11 +8,25 @@ namespace Izzimon.BloodDries
     public class Blood : Filth
     {
         // to add to the def to configure later
-        private static Color FreshBloodColor = new Color(131, 34, 34, 180);
-        private static Color DriedBloodColor = new Color(77, 54, 54, 180);
-        private static int DaysUntilFullyDry = 1;
+        private static int FreshBloodR = 131;
+        private static int FreshBloodG = 34;
+        private static int FreshBloodB = 34;
+
+        private static int DriedBloodR = 55;
+        private static int DriedBloodG = 24;
+        private static int DriedBloodB = 24;
+                
+        private static int DefaultAlpha = 180;
+
+        private static int StandardTemperature = 20;
+        private static int DaysUntilFullyDryAtStandardTemperature = 1;
+        private static int DaysUntilFullyEroded = 20;
+
 
         private float percentageDried = 0;
+        private float percentageEroded = 0;
+
+        private TerrainDef terrainType;
 
         public override void SpawnSetup(Map map, bool respawningAfterLoad)
         {
@@ -25,9 +39,10 @@ namespace Izzimon.BloodDries
             }
 
             // save a reference to the terrain type it is on
+            this.terrainType = base.Map.terrainGrid.TerrainAt(base.Position);
 
             // raise a message to ensure we've hooked into it correctly
-            MoteMaker.ThrowText(base.Position.ToVector3(), base.Map, "Blood spawned", 3f);
+            //MoteMaker.ThrowText(base.Position.ToVector3(), base.Map, "Blood spawned", 3f);
         }
 
 
@@ -35,10 +50,10 @@ namespace Izzimon.BloodDries
         {
             get
             {
-                var red = GetCurrentValue(FreshBloodColor.r, DriedBloodColor.r)/255;
-                var green = GetCurrentValue(FreshBloodColor.g, DriedBloodColor.g)/255;
-                var blue = GetCurrentValue(FreshBloodColor.b, DriedBloodColor.b)/255;
-                var alpha = 180 / 255f;
+                var red = GetWeightedAverage(FreshBloodR, DriedBloodR, percentageDried) / 255;
+                var green = GetWeightedAverage(FreshBloodG, DriedBloodG, percentageDried) / 255;
+                var blue = GetWeightedAverage(FreshBloodB, DriedBloodB, percentageDried) / 255;
+                var alpha = GetWeightedAverage(DefaultAlpha, 0, percentageEroded) / 255;
 
                 var color = new Color(red, green, blue, alpha);
 
@@ -73,22 +88,28 @@ namespace Izzimon.BloodDries
                 return;
             }
 
-            var percentageToDryPerHour = 2000f / 60000 * DaysUntilFullyDry;  // long tick = 2000 ticks.  one day in game time = 60000 ticks.
+            float temperatureModifier = 1f;
+            if (this.AmbientTemperature <= 0)
+            {
+                return;  // it is frozen, don't dry any more
+            }
+            else if (this.AmbientTemperature > StandardTemperature) {
+                temperatureModifier += (this.AmbientTemperature - StandardTemperature) / StandardTemperature;  // add a bonus if the temperature is hot
+            }
+                
+            var percentageMoreToDry = 2000f / (60000 * DaysUntilFullyDryAtStandardTemperature) * temperatureModifier;  // long tick = 2000 ticks.  one day in game time = 60000 ticks.
 
-            this.percentageDried += percentageToDryPerHour;
+            this.percentageDried += percentageMoreToDry;
             this.percentageDried = Math.Min(this.percentageDried, 1f);
 
-            this.Notify_ColorChanged(); // do we need this?
+            this.Notify_ColorChanged(); 
         }
 
-
-
-        private float GetCurrentValue(float from, float to)
+        private float GetWeightedAverage(float from, float to, float transitionPercentage)
         {
-            var difference = (to - from) * this.percentageDried;
+            var difference = (to - from) * transitionPercentage;
             return from + difference;
         }
-
 
     }
 }
--- BloodDries/1.3/Source/Izzimon.BloodDries/Graphic_Cluster.cs
+++ BloodDries/1.3/Source/Izzimon.BloodDries/Graphic_Cluster.cs
@@ -1,94 +1,14 @@
-﻿using RimWorld;
+﻿
 using UnityEngine;
 using Verse;
 
 namespace Izzimon.BloodDries
 {
-    public class Graphic_Cluster: Graphic_Collection
+    public class Graphic_Cluster: Verse.Graphic_Cluster
 	{
-		// Token: 0x170004E6 RID: 1254
-		// (get) Token: 0x06001848 RID: 6216 RVA: 0x00091481 File Offset: 0x0008F681
-		public override Material MatSingle
-		{
-			get
-			{
-				return this.subGraphics[Rand.Range(0, this.subGraphics.Length)].MatSingle;
-			}
-		}
-
-		// Token: 0x06001849 RID: 6217 RVA: 0x0009149D File Offset: 0x0008F69D
-		public override void DrawWorker(Vector3 loc, Rot4 rot, ThingDef thingDef, Thing thing, float extraRotation)
-		{
-			Log.ErrorOnce("Graphic_Scatter cannot draw realtime.", 9432243);
-		}
-
-		// Token: 0x0600184A RID: 6218 RVA: 0x000914B0 File Offset: 0x0008F6B0
-		public override void Print(SectionLayer layer, Thing thing, float extraRotation)
-		{
-			Vector3 a = thing.TrueCenter();
-			Rand.PushState();
-			Rand.Seed = thing.Position.GetHashCode();
-			Filth filth = thing as Filth;
-			int num;
-			if (filth == null)
-			{
-				num = 3;
-			}
-			else
-			{
-				num = filth.thickness;
-			}
-			for (int i = 0; i < num; i++)
-			{
-				Material matSingle = this.MatSingle;
-				Vector3 center = a + new Vector3(Rand.Range(-0.45f, 0.45f), 0f, Rand.Range(-0.45f, 0.45f));
-				Vector2 size = new Vector2(Rand.Range(this.data.drawSize.x * 0.8f, this.data.drawSize.x * 1.2f), Rand.Range(this.data.drawSize.y * 0.8f, this.data.drawSize.y * 1.2f));
-				float rot = (float)Rand.RangeInclusive(0, 360) + extraRotation;
-				bool flipUv = Rand.Value < 0.5f;
-				Vector2[] uvs;
-				Color32 color;
-				Graphic.TryGetTextureAtlasReplacementInfo(matSingle, thing.def.category.ToAtlasGroup(), flipUv, true, out matSingle, out uvs, out color);
-				Printer_Plane.PrintPlane(layer, center, size, matSingle, rot, flipUv, uvs, new Color32[]
-				{
-					color,
-					color,
-					color,
-					color
-				}, 0.01f, 0f);
-			}
-			Rand.PopState();
-		}
-
-		// Token: 0x0600184B RID: 6219 RVA: 0x00091638 File Offset: 0x0008F838
-		public override string ToString()
-		{
-			return string.Concat(new object[]
-			{
-				"Scatter(subGraphic[0]=",
-				this.subGraphics[0].ToString(),
-				", count=",
-				this.subGraphics.Length,
-				")"
-			});
-		}
-
-		// Token: 0x040010AB RID: 4267
-		private const float PositionVariance = 0.45f;
-
-		// Token: 0x040010AC RID: 4268
-		private const float SizeVariance = 0.2f;
-
-		// Token: 0x040010AD RID: 4269
-		private const float SizeFactorMin = 0.8f;
-
-		// Token: 0x040010AE RID: 4270
-		private const float SizeFactorMax = 1.2f;
-	
 
 		public override Graphic GetColoredVersion(Shader newShader, Color newColor, Color newColorTwo)
 		{
-			Log.Message("Entered GetColoredVersion with color: " + newColor.ToString());
-
 			var coloredVersion = new Graphic_Cluster();
 			var graphicRequest = new GraphicRequest()
 			{

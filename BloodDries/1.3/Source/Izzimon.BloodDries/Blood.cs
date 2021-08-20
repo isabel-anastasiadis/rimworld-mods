57091dce0f80fe30d46ebb1bee0863df3c533a84
Isabel Anastasiadis <anastasiadis.isabel@gmail.com>
2021-08-20 18:38:01 +1200

    Attempting to change the graphic to a smear when eroded, still need to test whether it works when loading from a save

--- BloodDries/1.3/Source/Izzimon.BloodDries/Blood.cs
+++ BloodDries/1.3/Source/Izzimon.BloodDries/Blood.cs
@@ -12,16 +12,15 @@ namespace Izzimon.BloodDries
         private static int FreshBloodG = 34;
         private static int FreshBloodB = 34;
 
-        private static int DriedBloodR = 55;
-        private static int DriedBloodG = 24;
-        private static int DriedBloodB = 24;
+        private static int DriedBloodR = 35;
+        private static int DriedBloodG = 14;
+        private static int DriedBloodB = 14;
                 
         private static int DefaultAlpha = 180;
+        private static int MinimumAlpha = 15;
 
         private static int StandardTemperature = 20;
-        private static int DaysUntilFullyDryAtStandardTemperature = 1;
-        private static int DaysUntilFullyEroded = 20;
-
+        private static int DaysUntilFullyDryAtStandardTemperature = 2;
 
         private float percentageDried = 0;
         private float percentageEroded = 0;
@@ -41,6 +40,11 @@ namespace Izzimon.BloodDries
             // save a reference to the terrain type it is on
             this.terrainType = base.Map.terrainGrid.TerrainAt(base.Position);
 
+            if (respawningAfterLoad && ShouldShowAsSmear()) {
+                this.Graphic.path = "Things/Filth/Grainy";
+            }
+
+
             // raise a message to ensure we've hooked into it correctly
             //MoteMaker.ThrowText(base.Position.ToVector3(), base.Map, "Blood spawned", 3f);
         }
@@ -53,7 +57,7 @@ namespace Izzimon.BloodDries
                 var red = GetWeightedAverage(FreshBloodR, DriedBloodR, percentageDried) / 255;
                 var green = GetWeightedAverage(FreshBloodG, DriedBloodG, percentageDried) / 255;
                 var blue = GetWeightedAverage(FreshBloodB, DriedBloodB, percentageDried) / 255;
-                var alpha = GetWeightedAverage(DefaultAlpha, 0, percentageEroded) / 255;
+                var alpha = GetWeightedAverage(DefaultAlpha, MinimumAlpha, percentageEroded) / 255;
 
                 var color = new Color(red, green, blue, alpha);
 
@@ -74,7 +78,21 @@ namespace Izzimon.BloodDries
 
         public override void TickLong()
         {
-            DryMore();
+            bool changed = false;
+            
+            changed |= DryMore();
+
+            changed |= ErodeMore();
+
+            if (percentageEroded == 1f)
+            {
+                this.Destroy(DestroyMode.Vanish);
+            }
+            else if (changed)
+            {
+                this.Notify_ColorChanged();
+            }
+
         }
 
         public override void ExposeData()
@@ -83,18 +101,18 @@ namespace Izzimon.BloodDries
             Scribe_Values.Look<float>(ref this.percentageDried, "percentageDried", 0f, false);
         }
 
-        private void DryMore() {
+        private bool DryMore() {
             if (this.percentageDried == 1f) {
-                return;
+                return false;
             }
 
             float temperatureModifier = 1f;
             if (this.AmbientTemperature <= 0)
             {
-                return;  // it is frozen, don't dry any more
+                return false;  // it is frozen, don't dry any more
             }
             else if (this.AmbientTemperature > StandardTemperature) {
-                temperatureModifier += (this.AmbientTemperature - StandardTemperature) / StandardTemperature;  // add a bonus if the temperature is hot
+                temperatureModifier += (this.AmbientTemperature - StandardTemperature)*2 / StandardTemperature;  // add a bonus if the temperature is hot
             }
                 
             var percentageMoreToDry = 2000f / (60000 * DaysUntilFullyDryAtStandardTemperature) * temperatureModifier;  // long tick = 2000 ticks.  one day in game time = 60000 ticks.
@@ -102,7 +120,29 @@ namespace Izzimon.BloodDries
             this.percentageDried += percentageMoreToDry;
             this.percentageDried = Math.Min(this.percentageDried, 1f);
 
-            this.Notify_ColorChanged(); 
+            return true;
+        }
+
+        private bool ErodeMore() {
+
+            var percentageMoreToErode = 2000f/ this.DisappearAfterTicks; 
+
+            if (this.terrainType.defName == "Sand" || this.terrainType.defName == "SoftSand" ) {
+                percentageMoreToErode *= 20;  // more erosion on soft terrains
+            }
+
+            this.percentageEroded += percentageMoreToErode;
+            this.percentageEroded = Math.Min(this.percentageEroded, 1f);
+
+            if (ShouldShowAsSmear()) {
+                this.Graphic.path = "Things/Filth/Grainy";
+            }
+            
+            return true;
+        }
+
+        private bool ShouldShowAsSmear() {
+            return this.percentageEroded > 0.33f;
         }
 
         private float GetWeightedAverage(float from, float to, float transitionPercentage)

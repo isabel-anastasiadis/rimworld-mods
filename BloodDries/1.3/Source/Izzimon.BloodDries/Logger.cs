adf3764102b87413b83c6b99b6394db835267dbd
Isabel Anastasiadis <anastasiadis.isabel@gmail.com>
2021-08-28 15:49:39 +1200

    removed the debug flag again, tests looking good

--- BloodDries/1.3/Source/Izzimon.BloodDries/Logger.cs
+++ BloodDries/1.3/Source/Izzimon.BloodDries/Logger.cs
@@ -5,7 +5,7 @@ namespace Izzimon.BloodDries
 {
     public static class Logger
     {
-        public static bool DebugEnabled = true;
+        public static bool DebugEnabled = false;
 
         public static void Debug(string message) {
             if (DebugEnabled) {
